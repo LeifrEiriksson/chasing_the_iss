43a3f4758dad9d93900da159686aac1efc99a685
João Vitor de Castro e Silva <j.v.decastroesilva@hotmail.com>
2023-05-16 18:26:11 -0300

    creating a new class with N2YO API

--- running.py
+++ running.py
@@ -1,10 +1,2 @@
 from Components import (Astrolabe, ImageOfDay, InternationalSpaceStation,
                         IssDataStreaming, My_Location)
-
-# me = My_Location.Where_Am_I()
-# iss = InternationalSpaceStation.Iss()
-# astrolabe = Astrolabe.Astrolabe()
-# image = ImageOfDay.ImageOfDay()
-
-data_streaming = IssDataStreaming
-data_streaming.ISS_Monitoring()
